9b5ee9a63e3b09ff7cc0f29840d98376b8eba0e0
daniyal5293 <daniyalkhan5293@gmail.com>
2024-08-11 21:32:05 +0500

    level0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import { useState } from "react";
+import Form from 'react-bootstrap/Form';
 
 function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [gender, setGender] = useState("");
+  const [superpower, setSuperpower] = useState("");
+  const [showData, setShowData] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form>
+        <div className="form-row">
+          <div className="form-group">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Type something…" onChange={(e) => setName(e.target.value)} />
+          </div>
+
+          <div className="form-group">
+            <Form.Label>Age</Form.Label>
+            <Form.Control type="number" onChange={(e) => setAge(e.target.value)} />
+          </div>
+
+          <div className="form-group">
+            <Form.Label>Gender</Form.Label>
+            <div className="form-check-inline">
+              <Form.Check type="radio" label="Male" value="Male" name="gender" onClick={() => setGender("Male")} />
+              <Form.Check type="radio" label="Female" value="Female" name="gender" onClick={() => setGender("Female")} />
+              <Form.Check type="radio" label="Other" value="Other" name="gender" onClick={() => setGender("Other")} />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <Form.Label>SuperPower</Form.Label>
+            <Form.Control type="text" placeholder="Type something…" onChange={(e) => setSuperpower(e.target.value)} />
+          </div>
+        </div>
+      </Form>
+
+      <Button variant="contained" onClick={() => setShowData(true)}>Submit</Button>
+
+      {showData && 
+        <div className='details'>
+          <h1>Details :</h1>
+          <h2>Name: {name}</h2>
+          <h2>Age: {age}</h2>
+          <h2>Gender: {gender}</h2>
+          <h2>SuperPower: {superpower}</h2>
+        </div>
+      }
     </div>
   );
 }
