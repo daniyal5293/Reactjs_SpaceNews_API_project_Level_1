cfab6c6996d7fcbedb7b0527c1d54e2d76722bb1
daniyal5293 <daniyalkhan5293@gmail.com>
2024-08-18 11:00:09 +0500

    api

--- src/App.js
+++ src/App.js
@@ -1,56 +1,34 @@
 import './App.css';
-import Button from '@mui/material/Button';
-import { useState } from "react";
-import Form from 'react-bootstrap/Form';
+import { useEffect, useState } from "react";
 
 function App() {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState(0);
-  const [gender, setGender] = useState("");
-  const [superpower, setSuperpower] = useState("");
-  const [showData, setShowData] = useState(false);
+  const [newsArticle, setNewsArticle] = useState([]);
+  useEffect(() => {
+    fetch("https://api.spaceflightnewsapi.net/v4/articles/").then(
+      (response) => response.json()).then((data) =>{
+     setNewsArticle(data.results);
+  
+    })
+  }, []);
 
   return (
-    <div className="App">
-      <Form>
-        <div className="form-row">
-          <div className="form-group">
-            <Form.Label>Name</Form.Label>
-            <Form.Control type="text" placeholder="Type something…" onChange={(e) => setName(e.target.value)} />
-          </div>
-
-          <div className="form-group">
-            <Form.Label>Age</Form.Label>
-            <Form.Control type="number" onChange={(e) => setAge(e.target.value)} />
-          </div>
-
-          <div className="form-group">
-            <Form.Label>Gender</Form.Label>
-            <div className="form-check-inline">
-              <Form.Check type="radio" label="Male" value="Male" name="gender" onClick={() => setGender("Male")} />
-              <Form.Check type="radio" label="Female" value="Female" name="gender" onClick={() => setGender("Female")} />
-              <Form.Check type="radio" label="Other" value="Other" name="gender" onClick={() => setGender("Other")} />
+   <div className="App">
+      <div className='top'>
+        <h1>Space News Articles</h1>
+      </div>
+      <div className='article_con'>
+        {newsArticle.map((val,key) => {
+          return (
+          <div className='article'>
+             <h3 key={key}>{val.title}</h3>
+              <img key={key} src={val.image_url} />
+              <p key={key}>summary: <br/>{val.summary}</p>
+              <h4 key={key}>Published:{val.published_at}</h4>
+              <h4 key={key}>Updated: {val.updated_at}</h4>
             </div>
-          </div>
-
-          <div className="form-group">
-            <Form.Label>SuperPower</Form.Label>
-            <Form.Control type="text" placeholder="Type something…" onChange={(e) => setSuperpower(e.target.value)} />
-          </div>
-        </div>
-      </Form>
-
-      <Button variant="contained" onClick={() => setShowData(true)}>Submit</Button>
-
-      {showData && 
-        <div className='details'>
-          <h1>Details :</h1>
-          <h2>Name: {name}</h2>
-          <h2>Age: {age}</h2>
-          <h2>Gender: {gender}</h2>
-          <h2>SuperPower: {superpower}</h2>
-        </div>
-      }
+          )
+        })}
+      </div>
     </div>
   );
 }
